cbaf83481710b0740a85477fa898035c1c57b777
Sukhendu <sukhendubhowmik53@gmail.com>
2023-06-16 11:13:08 +0530

    addPatient screen modification

--- src/main/java/com/prescription/prescriptioncreator/controller/AddPatientController.java
+++ src/main/java/com/prescription/prescriptioncreator/controller/AddPatientController.java
@@ -1,4 +1,30 @@
 package com.prescription.prescriptioncreator.controller;
 
+import com.prescription.prescriptioncreator.model.PatientDetails;
+import com.prescription.prescriptioncreator.util.DBUtil;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class AddPatientController {
+
+
+    @FXML
+    TextField txtFName;
+    @FXML
+
+    public void addPatientDetails ( ActionEvent event) throws SQLException {
+        String first_name=txtFName.getText();
+
+        System.out.println("AddPatient "+first_name);
+    }
 }

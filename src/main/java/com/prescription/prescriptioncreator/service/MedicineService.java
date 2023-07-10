bd41e3fbf6d70d7758017b7027ae4e4caeb90b3d
Nirmal Dhara <nirmal.dhara@gmail.com>
2023-07-10 22:54:18 +0530

    previous visit

--- src/main/java/com/prescription/prescriptioncreator/service/MedicineService.java
+++ src/main/java/com/prescription/prescriptioncreator/service/MedicineService.java
@@ -3,6 +3,10 @@ package com.prescription.prescriptioncreator.service;
 import com.prescription.prescriptioncreator.model.MedicineDetails;
 import com.prescription.prescriptioncreator.model.PatientDetails;
 
+import java.util.List;
+
 public interface MedicineService {
     void addMedicine(MedicineDetails medicineDetails) throws Exception;
-}
+     List<MedicineDetails> getAutoSuggestMedicine() throws Exception;
+
+    }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,12 +3,17 @@ module com.prescription.prescriptioncreator {
     requires javafx.fxml;
     requires java.sql;
     requires org.controlsfx.controls;
-
+    requires javafx.web;
+    requires javafx.graphics;
 
     opens com.prescription.prescriptioncreator to javafx.fxml;
     exports com.prescription.prescriptioncreator;
     exports com.prescription.prescriptioncreator.model;
+    exports  com.prescription.prescriptioncreator.util;
     opens com.prescription.prescriptioncreator.model to javafx.fxml;
     exports com.prescription.prescriptioncreator.controller;
     opens com.prescription.prescriptioncreator.controller to javafx.fxml;
+
+
 }
+

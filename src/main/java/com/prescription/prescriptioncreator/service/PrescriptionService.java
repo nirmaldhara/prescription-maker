c0d836df54f177375584a11c6a92ff9df5379b93
Nirmal Dhara <nirmal.dhara@gmail.com>
2023-07-14 22:28:44 +0530

    display old history

--- src/main/java/com/prescription/prescriptioncreator/service/PrescriptionService.java
+++ src/main/java/com/prescription/prescriptioncreator/service/PrescriptionService.java
@@ -2,7 +2,14 @@ package com.prescription.prescriptioncreator.service;
 
 import com.prescription.prescriptioncreator.model.MedicineDetails;
 import com.prescription.prescriptioncreator.model.PrescriptionDetails;
+import com.prescription.prescriptioncreator.model.PreviousVisit;
+
+import java.util.List;
 
 public interface PrescriptionService {
-    void addPrescription(MedicineDetails medicineDetails) throws Exception;//////
+
+    void saveNPrintPrescription( List<MedicineDetails> lstMedicineDetails, int patientId) throws Exception;
+    List<MedicineDetails>  getPrescriptionDetailsByVisitId( Integer visitID) throws Exception;
+    public List<PreviousVisit> getVisitDetails(int patientId) throws Exception;
+
 }

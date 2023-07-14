ff81511b8dbe55504a7a4f1d53e9a184dc00c749
Nirmal Dhara <nirmal.dhara@gmail.com>
2023-07-14 22:28:44 +0530

    display old history

--- src/main/java/com/prescription/prescriptioncreator/Dao/impl/PrescriptionDaoImpl.java
+++ src/main/java/com/prescription/prescriptioncreator/Dao/impl/PrescriptionDaoImpl.java
@@ -4,6 +4,7 @@ import com.prescription.prescriptioncreator.Dao.PrescriptionDao;
 import com.prescription.prescriptioncreator.model.MedicineDetails;
 import com.prescription.prescriptioncreator.model.PatientDetails;
 import com.prescription.prescriptioncreator.model.PrescriptionDetails;
+import com.prescription.prescriptioncreator.model.PreviousVisit;
 import com.prescription.prescriptioncreator.util.DBConnection;
 
 import java.sql.Connection;
@@ -16,25 +17,115 @@ import java.util.List;
 import static com.prescription.prescriptioncreator.util.DBConnection.getConnection;
 
 public class PrescriptionDaoImpl implements PrescriptionDao {
-    public void addPrescription(MedicineDetails medicineDetails) throws Exception{//////
-        String sql = " insert into prescription (medicine_name,when_bf_af,no_of_days,dose1, dose2, dose3, dose4, dose5, dose6,note) values (?, ?, ?, ?,?,?,?,?,?,?)";
-        Connection conn=getConnection();
-        try{
-            PreparedStatement preparedStmt = conn.prepareStatement(sql);
-            preparedStmt.setString(1,medicineDetails.getMedicineName());//
-            preparedStmt.setString(2,medicineDetails.getWhen());//
-            preparedStmt.setString(3,medicineDetails.getNoOfDays());//
 
-            preparedStmt.setString(4,medicineDetails.getDose1());//
-            preparedStmt.setString(5,medicineDetails.getDose2());//
-            preparedStmt.setString(6,medicineDetails.getDose3());//
-            preparedStmt.setString(7,medicineDetails.getDose4());//
-            preparedStmt.setString(8,medicineDetails.getDose5());//
-            preparedStmt.setString(9,medicineDetails.getDose6());//
-            preparedStmt.setString(10,medicineDetails.getNote());//
-            preparedStmt.execute();
-        }catch(Exception e){
-            throw new RuntimeException(e);
+    private int getLastVisitId() throws Exception {
+        String dbsql = "select IFNULL(max(visit_id),0)+1 visit_id from prescription;";
+        PreparedStatement preparedStmt =null;
+        ResultSet rs = null;
+        int visitId=0;
+        Connection conn = getConnection();
+        try{
+            preparedStmt=  conn.prepareStatement(dbsql);
+            rs = preparedStmt.executeQuery();
+            rs.next();
+            visitId=rs.getInt("visit_id");
+
+        } catch(Exception e){
+            e.printStackTrace();
         }
+        return visitId;
+    }
+
+    @Override
+    public void saveNPrintPrescription(List<MedicineDetails> lstMedicineDetails,int patientId) throws Exception {
+
+
+
+        String sql = " insert into prescription (visit_id, patient_id , medicine_name, when_bf_af, no_of_days,dose1,dose2,dose3,dose4,dose5,dose6,note) values (?, ?, ?, ?, ?,?,?,?, ?, ?, ?, ?)";
+        Connection conn=getConnection();
+        int visit_id=getLastVisitId();
+        for(MedicineDetails pd:lstMedicineDetails) {
+            try {
+                PreparedStatement preparedStmt = conn.prepareStatement(sql);
+                preparedStmt.setInt(1, visit_id);
+                preparedStmt.setInt(2,patientId );
+                preparedStmt.setString(3, pd.getMedicineName());
+                preparedStmt.setString(4, pd.getWhen());
+                preparedStmt.setInt(5, pd.getNoOfDays());
+                preparedStmt.setString(6,pd.getDose1());
+                preparedStmt.setString(7,pd.getDose2());
+                preparedStmt.setString(8,pd.getDose3());
+                preparedStmt.setString(9,pd.getDose4());
+                preparedStmt.setString(10,pd.getDose5());
+                preparedStmt.setString(11,pd.getDose6());
+                preparedStmt.setString(12, pd.getNote());
+                preparedStmt.execute();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public List<MedicineDetails> getPrescriptionDetailsByVisitId(Integer visitID) throws Exception {
+        String dbsql = "select visit_id, patient_id , medicine_name, when_bf_af, no_of_days,dose1,dose2,dose3,dose4,dose5,dose6,note from prescription where visit_id=?";
+        PreparedStatement preparedStmt =null;
+        ResultSet rs = null;
+        Connection conn = getConnection();
+        List<MedicineDetails>lstPrescription = new ArrayList<>();
+
+        try{
+            preparedStmt=  conn.prepareStatement(dbsql);
+            preparedStmt.setInt(1,visitID.intValue());
+            rs = preparedStmt.executeQuery();
+            while(  rs.next())
+            {
+                MedicineDetails medicineDetails =  new MedicineDetails();
+                medicineDetails.setMedicineName(rs.getString("medicine_name"));
+                medicineDetails.setWhen(rs.getString("when_bf_af"));
+                medicineDetails.setNoOfDays(rs.getInt("no_of_days"));
+                medicineDetails.setDose2(rs.getString("dose2"));
+                medicineDetails.setDose3(rs.getString("dose3"));
+                medicineDetails.setDose4(rs.getString("dose4"));
+                medicineDetails.setDose5(rs.getString("dose5"));
+                medicineDetails.setDose6(rs.getString("dose6"));
+                medicineDetails.setNote(rs.getString("note"));
+                lstPrescription.add(medicineDetails);
+
+            }
+
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return lstPrescription;
+    }
+
+    @Override
+    public List<PreviousVisit> getVisitDetails(int patientId) throws Exception {
+        String dbsql = "select  DATE_FORMAT(date,'%d/%m/%Y') AS visitdate, visit_id  from prescription where patient_id=?  group by DATE_FORMAT(date,'%d/%m/%Y'),visit_id";
+        PreparedStatement preparedStmt =null;
+        ResultSet rs = null;
+        int visitId=0;
+        Connection conn = getConnection();
+        List<PreviousVisit> visitList= new ArrayList<>();
+
+        try{
+            preparedStmt=  conn.prepareStatement(dbsql);
+            preparedStmt.setInt(1,patientId);
+            rs = preparedStmt.executeQuery();
+            while(rs.next()) {
+                PreviousVisit pv= new PreviousVisit();
+                pv.setPreviousVisit(rs.getString("visitdate"));
+           //     pv.setId(rs.getInt("id"));
+                pv.setVisitId(rs.getInt("visit_id"));
+                visitList.add(pv);
+
+            }
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return visitList;
     }
 }
--- src/main/java/com/prescription/prescriptioncreator/service/impl/PrescriptionServiceImpl.java
+++ src/main/java/com/prescription/prescriptioncreator/service/impl/PrescriptionServiceImpl.java
@@ -1,18 +1,31 @@
 package com.prescription.prescriptioncreator.service.impl;
 
-import com.prescription.prescriptioncreator.Dao.MedicineDao;
 import com.prescription.prescriptioncreator.Dao.PrescriptionDao;
-import com.prescription.prescriptioncreator.Dao.impl.MedicineDaoImpl;
 import com.prescription.prescriptioncreator.Dao.impl.PrescriptionDaoImpl;
 import com.prescription.prescriptioncreator.model.MedicineDetails;
 import com.prescription.prescriptioncreator.model.PrescriptionDetails;
+import com.prescription.prescriptioncreator.model.PreviousVisit;
 import com.prescription.prescriptioncreator.service.PrescriptionService;
 
-public class PrescriptionServiceImpl implements PrescriptionService {
-    public void addPrescription(MedicineDetails medicineDetails) throws Exception{//////
-        PrescriptionDao prescriptionDao = new PrescriptionDaoImpl();
-        prescriptionDao.addPrescription(medicineDetails);/////
-        ////////
+import java.util.List;
 
+public class PrescriptionServiceImpl implements PrescriptionService {
+    PrescriptionDao prescriptionDao= new PrescriptionDaoImpl();
+    @Override
+    public void saveNPrintPrescription(List<MedicineDetails> lstMedicineDetails, int patientId) throws Exception {
+        prescriptionDao.saveNPrintPrescription(lstMedicineDetails,patientId);
+    }
+
+    @Override
+    public List<MedicineDetails> getPrescriptionDetailsByVisitId(Integer visitID) throws Exception {
+        return prescriptionDao.getPrescriptionDetailsByVisitId(visitID);
+    }
+
+
+    @Override
+    public List<PreviousVisit> getVisitDetails(int patientId) throws Exception {
+
+
+        return prescriptionDao.getVisitDetails(patientId);
     }
 }

61d739df009fb2aa0b4eedd33b942fb8943c5e90
Nirmal Dhara <nirmal.dhara@gmail.com>
2023-07-16 22:40:09 +0530

    Validation for registration page

--- src/main/java/com/prescription/prescriptioncreator/controller/PatientController.java
+++ src/main/java/com/prescription/prescriptioncreator/controller/PatientController.java
@@ -1,9 +1,12 @@
 package com.prescription.prescriptioncreator.controller;
 
+import com.prescription.prescriptioncreator.Message;
 import com.prescription.prescriptioncreator.model.PatientDetails;
 import com.prescription.prescriptioncreator.service.PatientService;
 import com.prescription.prescriptioncreator.service.impl.PatientServiceImpl;
 import com.prescription.prescriptioncreator.util.ValidationUtil;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
@@ -19,7 +22,12 @@ public class PatientController {
     ComboBox<String> cmbSex;
     @FXML
     public void addPatientDetails ( ActionEvent event) throws Exception {
-        if(!ValidationUtil.isBlank(txtFName)){
+        if(!
+                ValidationUtil.isTextFieldBlank(txtFName, Message.PATIENT_FNAME_BlANK.val()) &&
+                ValidationUtil.isTextFieldBlank(txtLName, Message.PATIENT_LNAME_BlANK.val()) &&
+                ValidationUtil.isTextFieldBlank(txtAge, Message.PATIENT_AGE_BlANK.val()) &&
+                ValidationUtil.isComboBoxBlank(cmbSex, Message.PATIENT_SEX_BLANK.val())
+        ){
             PatientService patientService = new PatientServiceImpl();
             PatientDetails patientDetails = new PatientDetails();
             patientDetails.setFirst_name(txtFName.getText());
@@ -33,5 +41,20 @@ public class PatientController {
 
         //System.out.println("AddSex "+sex);
 
+    }
+    @FXML
+    public void initialize() throws Exception {
+
+        txtAge.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (newValue.matches("\\d*")) {
+                    int value = Integer.parseInt(newValue);
+                } else {
+                    txtAge.setText(oldValue);
+                }
+            }
+        });
     }
 }

d02da9e8aa10994a8b74916d71513ddc69b36f66
Nirmal Dhara <nirmal.dhara@gmail.com>
2023-07-18 21:43:54 +0530

    Toast message added

---
+++ src/main/java/com/prescription/prescriptioncreator/util/IntegerValue.java
@@ -0,0 +1,22 @@
+package com.prescription.prescriptioncreator.util;
+
+public enum IntegerValue {
+    LONG_DELAY(5000),
+    SHORT_DELAY(3000),
+    LONG_FADE_IN_DELAY(1000),
+    SHORT_FADE_IN_DELAY(500),
+    LONG_FADE_OUT_DELAY(1000),
+    SHORT_FADE_OUT_DELAY(500),
+    SUCCESS(200),
+    ERROR(500)
+    ;
+    private Integer value;
+
+    public Integer val() {
+        return value;
+    }
+
+    IntegerValue(Integer value){
+        this.value = value;
+    }
+}

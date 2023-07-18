1ea3823f5ed277478d1b96339d75c835e6000ffd
Nirmal Dhara <nirmal.dhara@gmail.com>
2023-07-18 21:43:54 +0530

    Toast message added

--- src/main/java/com/prescription/prescriptioncreator/controller/PatientController.java
+++ src/main/java/com/prescription/prescriptioncreator/controller/PatientController.java
@@ -4,14 +4,21 @@ import com.prescription.prescriptioncreator.Message;
 import com.prescription.prescriptioncreator.model.PatientDetails;
 import com.prescription.prescriptioncreator.service.PatientService;
 import com.prescription.prescriptioncreator.service.impl.PatientServiceImpl;
+import com.prescription.prescriptioncreator.util.ToastUtil;
 import com.prescription.prescriptioncreator.util.ValidationUtil;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import static com.prescription.prescriptioncreator.Message.ADD_PATIENT_SUCCESS;
+import static com.prescription.prescriptioncreator.util.IntegerValue.*;
 
 public class PatientController {
     @FXML
@@ -20,13 +27,14 @@ public class PatientController {
     TextArea txtAddress;
     @FXML
     ComboBox<String> cmbSex;
+    Stage stage = new Stage();
     @FXML
     public void addPatientDetails ( ActionEvent event) throws Exception {
-        if(!
+        if(!(
                 ValidationUtil.isTextFieldBlank(txtFName, Message.PATIENT_FNAME_BlANK.val()) &&
                 ValidationUtil.isTextFieldBlank(txtLName, Message.PATIENT_LNAME_BlANK.val()) &&
                 ValidationUtil.isTextFieldBlank(txtAge, Message.PATIENT_AGE_BlANK.val()) &&
-                ValidationUtil.isComboBoxBlank(cmbSex, Message.PATIENT_SEX_BLANK.val())
+                ValidationUtil.isComboBoxBlank(cmbSex, Message.PATIENT_SEX_BLANK.val()))
         ){
             PatientService patientService = new PatientServiceImpl();
             PatientDetails patientDetails = new PatientDetails();
@@ -36,7 +44,10 @@ public class PatientController {
             patientDetails.setMobile_no(txtMobileNo.getText());
             patientDetails.setAddress(txtAddress.getText());
             patientDetails.setAge(Integer.parseInt(txtAge.getText()));
-            patientService.addPatient(patientDetails);
+            if(patientService.addPatient(patientDetails)==true){
+                ToastUtil.makeText(stage, ADD_PATIENT_SUCCESS.val(), LONG_DELAY.val(), SHORT_FADE_IN_DELAY.val(), SHORT_FADE_OUT_DELAY.val(), SUCCESS.val());
+
+            }
         }
 
         //System.out.println("AddSex "+sex);
@@ -45,6 +56,8 @@ public class PatientController {
     @FXML
     public void initialize() throws Exception {
 
+        Scene scne= new Scene(new VBox());
+        stage.setScene(scne);
         txtAge.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue,
---
+++ src/main/java/com/prescription/prescriptioncreator/util/ToastUtil.java
@@ -0,0 +1,70 @@
+package com.prescription.prescriptioncreator.util;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+public class ToastUtil
+{
+    public static void makeText(Stage ownerStage, String toastMsg, int toastDelay, int fadeInDelay, int fadeOutDelay,int msgType)
+    {
+        Stage toastStage=new Stage();
+        toastStage.initOwner(ownerStage);
+        toastStage.setResizable(false);
+        toastStage.initStyle(StageStyle.TRANSPARENT);
+
+        Text text = new Text(toastMsg);
+        text.setFont(Font.font("Verdana", 12));
+        StackPane root = new StackPane(text);
+        if(msgType==200) {
+            text.setFill(Color.GREEN);
+
+            root.setStyle("-fx-background-radius: 5; -fx-background-color: #8af569; -fx-padding: 5px; -fx-border-style: solid; -fx-border-width: 2; -fx-border-color: #165c01;");
+
+        }
+        else if(msgType==500) {
+
+            root.setStyle("-fx-background-radius: 5; -fx-background-color: #f52a2a; -fx-padding: 5px;  -fx-border-style: solid; -fx-border-width: 2; -fx-border-color: #165c01;");
+
+            text.setFill(Color.RED);
+        }
+
+        root.setOpacity(0);
+
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        toastStage.setScene(scene);
+        toastStage.show();
+
+        Timeline fadeInTimeline = new Timeline();
+        KeyFrame fadeInKey1 = new KeyFrame(Duration.millis(fadeInDelay), new KeyValue (toastStage.getScene().getRoot().opacityProperty(), 1));
+        fadeInTimeline.getKeyFrames().add(fadeInKey1);
+        fadeInTimeline.setOnFinished((ae) ->
+        {
+            new Thread(() -> {
+                try
+                {
+                    Thread.sleep(toastDelay);
+                }
+                catch (InterruptedException e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                Timeline fadeOutTimeline = new Timeline();
+                KeyFrame fadeOutKey1 = new KeyFrame(Duration.millis(fadeOutDelay), new KeyValue (toastStage.getScene().getRoot().opacityProperty(), 0));
+                fadeOutTimeline.getKeyFrames().add(fadeOutKey1);
+                fadeOutTimeline.setOnFinished((aeb) -> toastStage.close());
+                fadeOutTimeline.play();
+            }).start();
+        });
+        fadeInTimeline.play();
+    }
+}

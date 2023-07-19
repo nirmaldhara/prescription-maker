48e793eb2d426eea8bc4983a8089a020bea2fae7
Sukhendu <sukhendubhowmik53@gmail.com>
2023-07-19 16:28:03 +0530

    Performed Medicine Validation and set the confirmation message

--- src/main/java/com/prescription/prescriptioncreator/Message.java
+++ src/main/java/com/prescription/prescriptioncreator/Message.java
@@ -5,7 +5,12 @@ public enum Message {
     PATIENT_LNAME_BlANK("Please provide the last name"),
     PATIENT_AGE_BlANK("Please provide the age"),
     PATIENT_SEX_BLANK("Please select the gender Male/Female"),
-    ADD_PATIENT_SUCCESS("Patient details added successfully");
+    ADD_PATIENT_SUCCESS("Patient details added successfully"),
+    MEDICINE_NAME_BLANK("Please provide the medicine name"),
+    MEDICINE_WHEN_BLANK("Please provide the medicine time BF/AF"),
+    MEDICINE_NO_OF_DAYS_BLANK("Please provide the number of days"),
+    ADD_MEDICINE_SUCCESS("Medicine details added successfully");
+
     private String value;
 
     public String val() {
